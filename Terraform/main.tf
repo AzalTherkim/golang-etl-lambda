a4ef806a0f4762a27f9649853b6aee056529a631
Philipp Thöne <philipp.tony@gmail.com>
2021-04-06 16:26:02 +0200

    added Terraform for lambda function

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,6 +12,57 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution" {
+
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+
+}
+
+
+resource "aws_lambda_permission" "allow_xml_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.transform_code.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.xml_data.arn
+}
+
+resource "aws_lambda_function" "transform_code" {
+  filename      = "main.zip"
+  function_name = "transform_code"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "main"
+  runtime       = "go1.x" 
+  environment {
+    variables = {
+      OUTPUT_BUCKET = aws_s3_bucket.json_data.id
+    }
+  }
+
+}
+
+
+
 data "aws_canonical_user_id" "current_user" {}
 
 resource "aws_s3_bucket" "xml_data" {
@@ -33,4 +84,15 @@ resource "aws_s3_bucket" "json_data" {
     permissions = ["FULL_CONTROL"]
   }
 
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.xml_data.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.transform_code.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_xml_bucket]
 }
